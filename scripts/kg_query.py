c6880f0df9e0f679b18fd0bdefcc8b853ee1aa1a
EIjo <eljo@live.nl>
2024-06-03 12:24:49 +0200

    added kg_query script to run a single kg query

---
+++ scripts/kg_query.py
@@ -0,0 +1,80 @@
+import neo4j_utils as nu
+import yaml
+from biochatter.prompts import BioCypherPromptEngine
+import json
+
+def jaccard_similarity(list1, list2):
+    s1 = set(list1)
+    s2 = set(list2)
+    return float(len(s1.intersection(s2)) / len(s1.union(s2)))
+
+
+URI = "bolt://localhost:7687"
+DB_NAME = None
+AUTH = None
+SCHEMA_FILE = "C:/Pistoia/BioCypher-OT/config/schema_config.yaml"
+
+DEPLOYMENT_NAME = 'gpt-4-turbo'
+MODEL_NAME = 'gpt-4-turbo'
+# DEPLOYMENT_NAME = 'gpt-35-turbo'
+# MODEL_NAME = 'gpt-35-turbo'
+
+
+neodriver = nu.Driver(
+        db_name=DB_NAME or "neo4j",
+        db_uri=URI,
+    )
+question = "what HumanGenes are related to Disease cancer?"
+gold_query = 'MATCH (g:HumanGene)-[:GeneToDiseaseAssociation]-(d:Disease {name: "cancer"}) RETURN  g'
+# how explicit should the questions be? humans implicitly have expectations they never explicitly say.
+
+gold_result = neodriver.query(gold_query)[0]
+print(len(gold_result))
+
+gold_elements = {}
+for e in gold_result:
+    for k, v in e.items():
+        if isinstance(v, dict):
+            for k1, v1 in v.items():
+                gold_elements[k1] = gold_elements.get(k1, set()) | {v1}
+        else:
+            gold_elements[k] = gold_elements.get(k, set()) | {v}
+
+with open(SCHEMA_FILE) as file:
+    schema_dict = yaml.safe_load(file)
+
+
+
+prompt_engine = BioCypherPromptEngine(
+    schema_config_or_info_dict=schema_dict,
+    model_name=MODEL_NAME,
+    deployment_name=DEPLOYMENT_NAME,
+                                      )
+
+query = prompt_engine.generate_query(question, 'Neo4j')
+
+llm_result = neodriver.query(query)[0]
+
+
+if not llm_result:
+    print('invalid query or no entities found')
+else:
+    llm_elements = {}
+
+    for e in llm_result:
+        if isinstance(e, dict):
+            for k, v in e.items():
+                if isinstance(v, dict):
+                    for k1, v1 in v.items():
+                        llm_elements[k1] = llm_elements.get(k1, set()) | {v1}
+                else:
+                    llm_elements[k] = llm_elements.get(k, set()) | {v}
+
+    for k, v in llm_elements.items():
+        print(k)
+        max_score = 0
+        for k1, v1 in gold_elements.items():
+            score = jaccard_similarity(v, v1)
+            if score > max_score:
+                max_score = score
+        print(max_score)
